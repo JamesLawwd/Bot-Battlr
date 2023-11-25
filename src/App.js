610ff864d55cd2881dec33add21cafb5d79b2cb5
JamesLawwd <jleo56291@gmail.com>
2023-11-25 23:14:49 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import BotCollection from './Component/BotCollection';
+
+function App() {
+  return (
+    <div >
+      <BotCollection />
+       
+    </div>
+  );
+}
+
+export default App;
